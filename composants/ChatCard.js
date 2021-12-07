cdcc605226134c0e2744d41fa9629da93f5bd29e
Edofo <Edofo@users.noreply.github.com>
2021-12-07 15:16:52 +0100

    update

---
+++ composants/ChatCard.js
@@ -0,0 +1,14 @@
+const ChatCard = () => {
+    return (
+        <div>
+            {/*IMAGE*/}
+            <div>
+                {/*NOM PRENOM*/}
+                {/*LAST MESSAGE*/}
+            </div>
+            {/*DATE*/}
+        </div>
+    )
+}
+
+export default ChatCard
---
+++ composants/SidebarContainer.js
@@ -0,0 +1,20 @@
+import ChatCard from "./ChatCard"
+
+const SidebarContainer = () => {
+    return (
+        <div>
+            <div>
+                {/*IMAGE*/}
+                <div>
+                    {/*NOM PRENOM*/}
+                    {/*STATUS*/}
+                </div>
+                {/*EDIT*/}
+            </div>
+            {/*SEARCH*/}
+            <ChatCard />
+        </div>
+    )
+}
+
+export default SidebarContainer
