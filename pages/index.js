fdbe8801e7df4db5218df3a3d02274c933f5cb56
Edofo <48387251+Edofo@users.noreply.github.com>
2021-12-08 08:55:08 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 import ChatContainer from '../composants/ChatContainer'
 import ProfilContainer from '../composants/ProfilContainer'
 import SidebarContainer from '../composants/SidebarContainer'
@@ -5,14 +7,26 @@ import SidebarContainer from '../composants/SidebarContainer'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
+    const { data: session } = useSession()
+
     return (
         <div className={styles.homeContainer}>
-      
-            <SidebarContainer />
 
-            <ChatContainer />
+            {
+                session ?
+                    <>
+                        <SidebarContainer />
 
-            <ProfilContainer />
+                        <ChatContainer />
+
+                        <ProfilContainer />
+                    </>
+                :
+                <>
+                    Not signed in <br />
+                    <button onClick={() => signIn()}>Sign in</button>
+                </>
+            }
 
         </div>
     )
